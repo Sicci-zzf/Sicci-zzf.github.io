2f3e12b193b832cb89c531fedb8603d818edda01
Sicci <zhangzhengfang2005@gmail.com>
2026-02-19 16:28:18 +0800

    fix: rewrite posts.js to use markdown slug

--- blog/posts.js
+++ blog/posts.js
@@ -2,7 +2,7 @@ const posts = [
   {
     title: "第一篇：你好，博客！",
     date: "2026-02-19",
-    url: "./posts/2026-02-19-hello.html",
+    slug: "2026-02-19-hello",
     desc: "我为什么做这个网站，以及接下来准备写什么。"
   }
 ];
@@ -13,7 +13,7 @@ if (!el) {
   console.error("找不到 #postList：请检查 blog/index.html 是否有 <div id='postList'>");
 } else {
   el.innerHTML = posts.map(p => `
-    <a class="postitem" href="${p.url}">
+    <a class="postitem" href="/blog/post.html?p=${encodeURIComponent(p.slug)}">
       <div class="postmeta">
         <div class="posttitle">${p.title}</div>
         <div class="postdesc">${p.desc}</div>
